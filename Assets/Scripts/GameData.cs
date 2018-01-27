f8ee43dc125a98826a0ac1384e04cc4990549192
ukjolie2 <ukjolie2@gmail.com>
2018-01-27 10:20:34 -0700

    Created GameData

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+
+[CreateAssetMenu(fileName = "GameData")]
+public class GameData : ScriptableObject {
+
+    public int poopSize { get; private set; }
+    public int score { get; private set; }
+    public Timer clock { get; private set; }
+    public int npcCount { get; private set; }
+
+    //Poop
+
+    public void updatePoop(int size)
+    {
+        poopSize = size;
+    }
+
+    //change score
+    public void updateScore(int increment)
+    {
+        score += increment;
+    }
+
+    //Time controls
+    public void createTimer()
+    {
+        clock = new Timer();
+    }
+
+    public void startTimer()
+    {
+        clock.Start();
+    }
+
+    public void stopTimer()
+    {
+        clock.Stop();
+    }
+
+    //NPC data
+    public void updateNPCCount(int increment)
+    {
+        npcCount += increment;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameData data;
+  
+	// Use this for initialization
+	void Start () {
+        data.createTimer();
+        //load map
+        //create npcs/update npc count
+        //create first pickup in visible area (probably same area each time)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//have arrow point at dest.
+	}
+
+    void createNPC(int amount)
+    {
+
+    }
+
+    //updates arrow direction
+    void updateArrow(GameObject npc)
+    {
+
+    }
+
+    void createPickUp()
+    {
+        //create 
+    }
+}
