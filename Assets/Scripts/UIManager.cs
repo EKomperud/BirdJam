abca3d910d07086b6d2511071a84bf5864251be1
Kevan Rahav <kevan.or@gmail.com>
2018-01-27 14:23:35 -0700

    fixed sorting order, pause stops clock as well

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Diagnostics;
 
 public class UIManager : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class UIManager : MonoBehaviour {
     public GameData gData;
     public Text poopText;
     public Image poopAlertPanel;
+    //temp until fused with game manager
+    public Stopwatch clock;
     // Use this for initialization
     void Start()
     {
@@ -19,8 +22,10 @@ public class UIManager : MonoBehaviour {
         hidePaused();
         //setting alpha channel to 0
         SetTranspernecy(0);
-        gData.createTimer();
-        gData.startTimer();
+        //gData.createTimer();
+        //gData.startTimer();
+        clock = new Stopwatch();
+        clock.Start();
     }
 
     // Update is called once per frame
@@ -30,15 +35,17 @@ public class UIManager : MonoBehaviour {
         //uses the p button to pause and unpause the game
         if ((Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)) && SceneManager.GetActiveScene().name != "MainMenu")
         {
-            if (Time.timeScale == 1)
+            if (Mathf.Equals(Time.timeScale, 1f))
             {
                 Time.timeScale = 0;
+                clock.Stop();
                 showPaused();
             }
             else if (Time.timeScale == 0)
             {
-                Debug.Log("high");
+                //Debug.Log("high");
                 Time.timeScale = 1;
+                clock.Start();
                 hidePaused();
             }
         }
@@ -47,7 +54,7 @@ public class UIManager : MonoBehaviour {
     //controls the pausing of the scene
     public void pauseControl()
     {
-        if (Time.timeScale == 1)
+        if (Mathf.Equals(Time.timeScale, 1f))
         {
             Time.timeScale = 0;
             showPaused();
@@ -95,8 +102,8 @@ public class UIManager : MonoBehaviour {
     public void UpdateGUI()
     {
         //poopText.text = gData.poopSize.ToString() ;
-        poopText.text = ((int)(gData.clock.ElapsedMilliseconds / 1000)).ToString();
-        SetTranspernecy(gData.clock.ElapsedMilliseconds / 10000.0f);
+        poopText.text = ((int)(clock.ElapsedMilliseconds / 1000)).ToString();
+        SetTranspernecy(clock.ElapsedMilliseconds / 10000.0f);
         
     }
 
@@ -105,7 +112,7 @@ public class UIManager : MonoBehaviour {
     {
         Color colorHolder;
         colorHolder = poopAlertPanel.color;
-        Debug.Log(colorHolder.a);
+        //Debug.Log(colorHolder.a);
         float clampalpah = Mathf.Clamp(transLevel, 0f, 1f);
         colorHolder.a = clampalpah;
         poopAlertPanel.color = colorHolder;
