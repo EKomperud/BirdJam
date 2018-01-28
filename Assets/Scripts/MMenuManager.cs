3f4616163a0daa213102cc34839d52c83ac6f857
Kevan Rahav <kevan.or@gmail.com>
2018-01-27 17:36:38 -0700

    Main Menu

--- Assets/Scripts/MMenuManager.cs
+++ Assets/Scripts/MMenuManager.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using System.Diagnostics;
 
-public class UIManager : MonoBehaviour {
+
+public class MMenuManager : MonoBehaviour {
 
     //used https://www.sitepoint.com/building-a-dodger-game-clone-in-unity/
     GameObject[] pauseObjects;
-    public GameData gData;
+    GameObject[] instructionObjects;
     //public Text poopText;
     public Image poopAlertPanel;
     //temp until fused with game manager
-    public Stopwatch clock;
     public Slider poopSlider;
     public Slider timeSlider;
     public Image orbCounter;
     // Use this for initialization
     void Start()
     {
-        Time.timeScale = 1;
+        //Time.timeScale = 1;
         pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
-        hidePaused();
+        instructionObjects = GameObject.FindGameObjectsWithTag("ShowInstructions");
+
+        //hidePaused();
+        showPaused();
         //setting alpha channel to 0
-        SetTranspernecy(0);
+        //SetTranspernecy(0);
         //gData.createTimer();
         //gData.startTimer();
-        clock = new Stopwatch();
-        clock.Start();
-        poopSlider.value = 0;
-        timeSlider.value = 100;
+        poopSlider.value = 40;
+        timeSlider.value = 60;
 
         //orbCounter.color
     }
@@ -38,40 +38,9 @@ public class UIManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        UpdateGUI();
-        //uses the p button to pause and unpause the game
-        if ((Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)) && SceneManager.GetActiveScene().name != "MainMenu")
-        {
-            if (Mathf.Equals(Time.timeScale, 1f))
-            {
-                Time.timeScale = 0;
-                clock.Stop();
-                showPaused();
-            }
-            else if (Time.timeScale == 0)
-            {
-                //Debug.Log("high");
-                Time.timeScale = 1;
-                clock.Start();
-                hidePaused();
-            }
-        }
+
     }
 
-    //controls the pausing of the scene
-    public void pauseControl()
-    {
-        if (Mathf.Equals(Time.timeScale, 1f))
-        {
-            Time.timeScale = 0;
-            showPaused();
-        }
-        else if (Time.timeScale == 0)
-        {
-            Time.timeScale = 1;
-            hidePaused();
-        }
-    }
 
     //shows objects with ShowOnPause tag
     public void showPaused()
@@ -80,15 +49,27 @@ public class UIManager : MonoBehaviour {
         {
             g.SetActive(true);
         }
+
+        foreach (GameObject i in instructionObjects)
+        {
+            i.SetActive(false);
+        }
     }
 
     //hides objects with ShowOnPause tag
     public void hidePaused()
     {
+        foreach (GameObject i in instructionObjects)
+        {
+            i.SetActive(true);
+        }
+
         foreach (GameObject g in pauseObjects)
         {
             g.SetActive(false);
         }
+
+
     }
 
     //loads inputted level
@@ -98,27 +79,7 @@ public class UIManager : MonoBehaviour {
         SceneManager.LoadScene(level);
     }
 
-    public void RestartLevel()
-    {
-        //Application.LoadLevel(Application.loadedLevel);
-        Scene currentScene = SceneManager.GetActiveScene();
-        LoadLevel(currentScene.name);
-        //SceneManager.LoadScene(SceneManager.sce);
-    }
-
-    public void UpdateGUI()
-    {
-        //poopText.text = gData.poopSize.ToString() ;
-        float clocknumtest = clock.ElapsedMilliseconds / 10000.0f;
-        //poopText.text = ((int)(clock.ElapsedMilliseconds / 1000)).ToString();
-        SetTranspernecy(clock.ElapsedMilliseconds / 10000.0f);
-        poopSlider.value = ((int)(clock.ElapsedMilliseconds / 100));
-        timeSlider.value = 100 - (int)(clock.ElapsedMilliseconds / 100);
-        SetOrbColor(clocknumtest,clocknumtest);
-    }
-
-
-    public void SetOrbColor(float green,float blue)
+    public void SetOrbColor(float green, float blue)
     {
         Color colorHolder = orbCounter.color;
         //Debug.Log(colorHolder.a);
@@ -128,9 +89,9 @@ public class UIManager : MonoBehaviour {
         {
             colorHolder.g -= green;
         }
-        if(colorHolder.b>0)
+        if (colorHolder.b > 0)
         {
-           colorHolder.b -= blue;
+            colorHolder.b -= blue;
         }
         orbCounter.color = colorHolder;
     }
