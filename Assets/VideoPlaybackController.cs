f17a8d91981c99e19b27aa719d64a35f19e426b4
MilkTicc <u0940550@utah.edu>
2018-01-28 12:48:58 -0700

    Update Intro Scene

---
+++ Assets/VideoPlaybackController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlaybackController : MonoBehaviour {
+
+
+    public VideoClip[] videos;
+    VideoPlayer vPlayer;
+
+	// Use this for initialization
+	void Start () {
+        vPlayer = GetComponent<VideoPlayer>();
+        StartCoroutine(PlayIntroVideo());
+
+	}
+
+    IEnumerator PlayIntroVideo()
+    {
+        vPlayer.clip = videos[0];
+        vPlayer.Play();
+
+        yield return new WaitForSeconds((float)vPlayer.clip.length - .5f);
+        while (true){
+            
+            vPlayer.time = 7;
+            vPlayer.isLooping = true;
+            yield return new WaitForSeconds((float)vPlayer.clip.length - 7);
+
+    }
+        //vPlayer.clip = videos[1];
+        //vPlayer.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
